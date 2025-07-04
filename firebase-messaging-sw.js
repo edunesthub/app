34818d868668799bef60513872a918ea3297862f
Gabriel Akpagli <bssidd241792@mug.edu.gh>
2025-07-04 19:17:30 +0000

    a

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,19 +1,16 @@
-// Import Firebase scripts
-importScripts('https://www.gstatic.com/firebasejs/10.9.0/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/10.9.0/firebase-messaging.js');
+importScripts('https://www.gstatic.com/firebasejs/9.22.2/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/9.22.2/firebase-messaging-compat.js');
 
-// Initialize Firebase app inside Service Worker
 firebase.initializeApp({
   apiKey: "AIzaSyADvpUQWo75ExePGoCRirD2mM-lmfM4Cmc",
-        authDomain: "von600-7982d.firebaseapp.com",
-        projectId: "von600-7982d",
-        storageBucket: "von600-7982d.appspot.com",
-        messagingSenderId: "164591218045",
-        appId: "1:164591218045:web:afe17512e16573e7903014",
-        measurementId: "G-E69DMPLXBK"
+  authDomain: "von600-7982d.firebaseapp.com",
+  projectId: "von600-7982d",
+  storageBucket: "von600-7982d.appspot.com",
+  messagingSenderId: "164591218045",
+  appId: "1:164591218045:web:afe17512e16573e7903014",
+  measurementId: "G-E69DMPLXBK"
 });
 
-// Retrieve an instance of Firebase Messaging
 const messaging = firebase.messaging();
 
 messaging.onBackgroundMessage(function(payload) {
