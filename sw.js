b07571f66ba1c829577ab777514fa0d9fed7d9dc
havoschools <havoagenciesltd@gmail.com>
2025-07-30 23:01:03 +0000

    a

--- sw.js
+++ sw.js
@@ -3,33 +3,51 @@ importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.5.4/workbox
 
 // 🔁 Precache all critical files (edit paths if needed)
 workbox.precaching.precacheAndRoute([
-  { url: '/index.html', revision: '1a' },
-  { url: '/loading.html', revision: '1a' },
-  { url: '/welcome.html', revision: '1a' },
-  { url: '/home.html', revision: '1a' },
-  { url: '/profile.html', revision: '1a' },
-  { url: '/cart.html', revision: '1a' },
-  { url: '/login.html', revision: '1a' },
-  { url: '/signup.html', revision: '1a' },
-  { url: '/refund.html', revision: '1a' },
-  { url: '/restaurant.html', revision: '1a' },
-  { url: '/contact-us.html', revision: '1a' },
-  { url: '/privacy.html', revision: '1a' },
-  { url: '/terms.html', revision: '1a' },
-  { url: '/orders.html', revision: '1a' },
-  { url: '/css/style.css', revision: '1a' },
-  { url: '/css/index.css', revision: '1a' },
-  { url: '/css/home.css', revision: '1a' },
-  { url: '/js/index.js', revision: '1a' },
-  { url: '/js/home.js', revision: '1a' },
-  { url: '/js/profile.js', revision: '1a' },
-  { url: '/js/cart.js', revision: '1a' },
-  { url: '/js/orders.js', revision: '1a' },
-  { url: '/register-sw.js', revision: '1a' },
-  { url: '/manifest.json', revision: '1a' },
-  { url: '/img/icon-192x192.png', revision: '1a' },
-  { url: '/img/icon-512x512.png', revision: '1a' },
-  { url: '/offline.html', revision: '1a' },
+  // 🔸 HTML pages
+  { url: '/index.html', revision: '1aa' },
+  { url: '/loading.html', revision: '1aa' },
+  { url: '/welcome.html', revision: '1aa' },
+  { url: '/home.html', revision: '1aa' },
+  { url: '/profile.html', revision: '1aa' },
+  { url: '/cart.html', revision: '1aa' },
+  { url: '/login.html', revision: '1aa' },
+  { url: '/signup.html', revision: '1aa' },
+  { url: '/refund.html', revision: '1aa' },
+  { url: '/restaurant.html', revision: '1aa' },
+  { url: '/contact-us.html', revision: '1aa' },
+  { url: '/privacy.html', revision: '1aa' },
+  { url: '/terms.html', revision: '1aa' },
+  { url: '/orders.html', revision: '1aa' },
+  { url: '/offline.html', revision: '1aa' },
+
+  // 🔸 CSS files
+  { url: '/css/style.css', revision: '1aa' },
+  { url: '/css/index.css', revision: '1aa' },
+  { url: '/css/home.css', revision: '1aa' },
+  { url: '/css/profile.css', revision: '1aa' },
+  { url: '/css/cart.css', revision: '1aa' },
+  { url: '/css/orders.css', revision: '1aa' },
+    { url: '/css/login.css', revision: '1aa' },
+    { url: '/css/signup.css', revision: '1aa' },
+    { url: '/css/restaurant.css', revision: '1aa' },
+
+  // 🔸 JS files
+  { url: '/js/index.js', revision: '1aa' },
+  { url: '/js/home.js', revision: '1aa' },
+  { url: '/js/profile.js', revision: '1aa' },
+  { url: '/js/cart.js', revision: '1aa' },
+  { url: '/js/orders.js', revision: '1aa' },
+  { url: '/register-sw.js', revision: '1aa' },
+    { url: '/js/login.js', revision: '1aa' },
+    { url: '/js/signup.js', revision: '1aa' },
+    { url: '/js/restaurant.js', revision: '1aa' },
+
+  // 🔸 Manifest & Service Worker
+  { url: '/manifest.json', revision: '1aa' },
+
+  // 🔸 Icons & Images
+  { url: '/img/icon-192x192.png', revision: '1aa' },
+  { url: '/img/icon-512x512.png', revision: '1aa' },
 ]);
 
 // ⚡ Cache-first for HTML pages (instant load, still updates when online)
